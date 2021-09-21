89fd822647fafe05f03a7ca99a44c2b6699fefd8
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-21 13:56:10 +1000

    added test script

---
+++ aerial_imagery/swimming_pools/testing/xx_run_python_script_via_ssh.sh
@@ -0,0 +1,16 @@
+
+# get directory this script is running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# load AWS vars
+. ${GIT_HOME}/temp_ec2_vars.sh
+
+# copy and run python script remotely
+FILENAME="../04_load_training_data_to_postgres.py"
+
+scp -F ${SSH_CONFIG} ${SCRIPT_DIR}/${FILENAME} ${USER}@${INSTANCE_ID}:~/
+ssh -F ${SSH_CONFIG} ${INSTANCE_ID} "conda activate yolov5; python3 ${FILENAME}"
+
+
+
+#ssh -F ${SSH_CONFIG} ${INSTANCE_ID}
