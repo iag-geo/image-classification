58c97d6b5a442abef60a3bc228ef417f300c40ce
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-21 19:22:43 +1000

    added output labels

--- aerial_imagery/swimming_pools/xx_code_scraps.sh
+++ aerial_imagery/swimming_pools/xx_code_scraps.sh
@@ -30,12 +30,13 @@ aws s3 cp s3://image-classification-swimming-pools/model/ ${HOME}/tmp/image-clas
 # run inference
 cp ~/datasets/pool/images/train2017/merged-gordon-19_151.143_-33.76.tif ~/detect/test_image.tif
 
-python3 ~/yolov5/detect.py --img 640 --source ~/detect/test_image.tif --weights ~/yolov5/runs/train/exp5/weights/best.pt --conf-thres 0.4
+python3 ~/yolov5/detect.py --img 640 --source ~/detect/test_image.tif --weights ~/yolov5/runs/train/exp5/weights/best.pt --conf-thres 0.4 --save-txt
 
 #Model Summary: 224 layers, 7053910 parameters, 0 gradients, 16.3 GFLOPs
 #image 1/1 /home/ec2-user/detect/test_image.tif: 640x640 6 pools, Done. (0.015s)
 #Speed: 0.6ms pre-process, 14.5ms inference, 1.3ms NMS per image at shape (1, 3, 640, 640)
 #Results saved to runs/detect/exp2
 
-# copy result locally
-scp -F ${SSH_CONFIG} ${USER}@${INSTANCE_ID}:~/runs/detect/exp2/test_image.tif ${SCRIPT_DIR}/testing/test_image.tif
+# copy results locally
+scp -F ${SSH_CONFIG} ${USER}@${INSTANCE_ID}:~/runs/detect/exp3/test_image.tif ${SCRIPT_DIR}/testing/test_image.tif
+scp -F ${SSH_CONFIG} ${USER}@${INSTANCE_ID}:~/runs/detect/exp3/labels/* ${SCRIPT_DIR}/testing/labels/
