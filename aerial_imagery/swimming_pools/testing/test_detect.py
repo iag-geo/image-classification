c5cc58b01f1c663b1fb80bdfa4be1728bc4aee81
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-22 12:23:47 +1000

    detect as a Python function working

--- aerial_imagery/swimming_pools/testing/test_detect.py
+++ aerial_imagery/swimming_pools/testing/test_detect.py
@@ -1,18 +1,27 @@
 
 import torch
 
-# Model
-model = torch.hub.load("/Users/s57405/tmp/image-classification/model/weights", 'best', pretrained=True)
+from datetime import datetime
 
-# Images
+start_time = datetime.now()
+
+# load trained pool model
+model = torch.hub.load("/Users/s57405/git/yolov5", "custom", path="/Users/s57405/tmp/image-classification/model/weights/best.pt", source="local")  # local repo
+
+print(f"Model loaded : {datetime.now() - start_time}")
+start_time = datetime.now()
+
+# Get image to process
 imgs = ["/Users/s57405/Downloads/Swimming Pools with Labels/chips_gordon_19/merged-gordon-19_151.143_-33.76.tif"]  # batch of images
 
 # Inference
 results = model(imgs)
 
-# Results
-# results.print()
-# results.save()  # or .show()
+print(f"Image processed : {datetime.now() - start_time}")
 
-print(results.xyxy[0])  # img1 predictions (tensor)
-# results.pandas().xyxy[0]  # img1 predictions (pandas)
+# Results
+results.print()
+results.save()  # or .show()
+
+# print(results.xyxy[0])  # img1 predictions (tensor)
+print(results.pandas().xyxy[0])  # img1 predictions (pandas)
